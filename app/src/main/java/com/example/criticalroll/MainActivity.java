c27f1ec1e03361a19aa92a32b27e76706f7344b3
Alexander Burgos <67289166+AlexB-arch@users.noreply.github.com>
2022-09-21 18:45:01 -0500

    Expands switch cases to 20. Adds critical message toggle function.

--- app/src/main/java/com/example/criticalroll/MainActivity.java
+++ app/src/main/java/com/example/criticalroll/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.criticalroll;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -13,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
     //Initiate random number generator class.
     Random rngeezus = new Random();
 
+    //Views
     ImageView die;
+    TextView crit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
         //Image View for with the die
         die = findViewById(R.id.die_face_image);
-        die.setImageResource(R.drawable.dice1);
+        die.setImageResource(R.drawable.d20_20);
+
+        //Text View for critical message
+        crit = findViewById(R.id.crit_message);
 
         //onClick listener
         die.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void rollDice(){
         int rng = rngeezus.nextInt(6) + 1;
+
+        toggleCritMessage(rng);
 
         switch (rng){
             case 1:
@@ -55,6 +64,64 @@ public class MainActivity extends AppCompatActivity {
             case 6:
                 die.setImageResource(R.drawable.dice6);
                 break;
+            case 7:
+                die.setImageResource(R.drawable.dice5);
+                break;
+            case 8:
+                die.setImageResource(R.drawable.dice5);
+                break;
+            case 9:
+                die.setImageResource(R.drawable.dice5);
+                break;
+            case 10:
+                die.setImageResource(R.drawable.dice5);
+                break;
+            case 11:
+                die.setImageResource(R.drawable.dice5);
+                break;
+            case 12:
+                die.setImageResource(R.drawable.dice5);
+                break;
+            case 13:
+                die.setImageResource(R.drawable.dice5);
+                break;
+            case 14:
+                die.setImageResource(R.drawable.dice5);
+                break;
+            case 15:
+                die.setImageResource(R.drawable.dice5);
+                break;
+            case 16:
+                die.setImageResource(R.drawable.dice5);
+                break;
+            case 17:
+                die.setImageResource(R.drawable.dice5);
+                break;
+            case 18:
+                die.setImageResource(R.drawable.dice5);
+                break;
+            case 19:
+                die.setImageResource(R.drawable.dice5);
+                break;
+            case 20:
+                die.setImageResource(R.drawable.dice5);
+                break;
         }
     }
+
+    //Only display the message if the dice is 1 or 20.
+    public void toggleCritMessage(int toggle){
+        if (toggle == 1){
+            crit.setText(getString(R.string.crit_miss));
+            crit.setVisibility(View.VISIBLE);
+        }
+        else if (toggle == 20){
+            crit.setText(getString(R.string.crit_hit));
+            crit.setVisibility(View.VISIBLE);
+        }
+        else{
+            crit.setVisibility(View.INVISIBLE);
+        }
+    }
+
 }
